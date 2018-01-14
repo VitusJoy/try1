38c77a3be7ea8bbbe166fc129c897e018dfa8f41
vj4407 <vj4407@gmail.com>
2018-01-14 18:01:18 +0530

    2

--- app/src/main/java/com/login/vitus/try1/Activity2.java
+++ app/src/main/java/com/login/vitus/try1/Activity2.java
@@ -2,6 +2,7 @@ package com.login.vitus.try1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class Activity2 extends AppCompatActivity {
@@ -17,5 +18,14 @@ public class Activity2 extends AppCompatActivity {
         ETusename = (EditText)findViewById(R.id.username1);
         ETpassword = (EditText)findViewById(R.id.password1);
 
+    }
+
+    public void OnLogin(View view){
+        String username = ETusename.getText().toString();
+        String password = ETpassword.getText().toString();
+        String type = "login";
+
+        BackgroundWorker bgw = new BackgroundWorker(this);
+        bgw.execute(type,username,password);
     }
 }
